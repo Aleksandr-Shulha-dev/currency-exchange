b43db422964c2aa4f98535f1d8213646d88ac49a
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-09-27 15:43:54 +0300

    added header to app component

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
-import './style.scss';
+import { Header } from '../common/header';
+import './styles.scss';
 
 const App: FC = () => {
   return (
     <div className='app'>
+      <Header></Header>
     </div>
   )
 }
