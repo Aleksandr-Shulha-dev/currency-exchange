2ceb1a8570ec9dbd94a4d5e89039f61d8a6384c8
Aleksandr-Shulha-dev <aleksandr.shulha.dev@gmail.com>
2022-09-27 15:42:51 +0300

    created exchange rate component

---
+++ src/components/common/exchange-rate/index.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { ExchangeRateProps } from '../../../common/types';
+import euFlag from '../../../assets/image/eu.png';
+import usaFlag from '../../../assets/image/usa.png';
+import btcFlag from '../../../assets/image/btc.png';
+
+import './styles.scss';
+ 
+const ExchangeRate: FC<ExchangeRateProps> = ({ data }) => {
+  const {ccy, buy, sale} = data;
+  const flag = ccy === "USD" ? usaFlag : ccy === "EUR" ? euFlag : btcFlag;
+  return (
+    <div className='rate'>
+      <span className='flag'>
+        <img src={flag} alt="coin" />
+      </span>
+      <span>{ccy}</span>
+      <span>{(+buy).toFixed(2)}</span>
+      <span className='slash'>/</span>
+      <span>{(+sale).toFixed(2)}</span>
+    </div>
+  );
+}
+ 
+export { ExchangeRate };
